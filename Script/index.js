b5657544405c3fb452370dcd14f388c8e68fa01f
xq915e <xq915e@A6096046>
2019-12-26 00:33:32 +0530

    added restricting movements

--- Script/index.js
+++ Script/index.js
@@ -1,23 +1,72 @@
 document.onkeydown = Movement;
 
 
-function Movement(e){
+function Movement(e) {
 
     e = e || window.event;
     var element = document.getElementById('characterIcon'),
-    style = window.getComputedStyle(element);    
+        style = window.getComputedStyle(element);
     const crrLeft = style.getPropertyValue('left');
     const crrTop = style.getPropertyValue('top');
+
     if (e.keyCode == '38') {//up      
-        document.getElementById('characterIcon').style.top = parseInt(crrTop) - 30;
+        var valid = validateMovement(crrLeft, crrTop, 'up')
+        if (valid) {
+            document.getElementById('characterIcon').style.top = parseInt(crrTop) - 30;
+        }
+
     }
     else if (e.keyCode == '40') {//down
-        document.getElementById('characterIcon').style.top = parseInt(crrTop) + 30;
+        var valid = validateMovement(crrLeft, crrTop, 'down')
+        if (valid) {
+
+            document.getElementById('characterIcon').style.top = parseInt(crrTop) + 30;
+        }
+
     }
     else if (e.keyCode == '37') {//left
-        document.getElementById('characterIcon').style.left = parseInt(crrLeft) - 30;
+        var valid = validateMovement(crrLeft, crrTop, 'left')
+        if (valid) {
+
+            document.getElementById('characterIcon').style.left = parseInt(crrLeft) - 30;
+        }
+
     }
     else if (e.keyCode == '39') {//right
-        document.getElementById('characterIcon').style.left = parseInt(crrLeft) + 30;
+
+        var valid = validateMovement(crrLeft, crrTop, 'right')
+        if (valid) {
+            document.getElementById('characterIcon').style.left = parseInt(crrLeft) + 30;
+        }
+
     }
+
+
+
+}
+
+function validateMovement(left, top, action) {
+
+    let validate = true;
+
+    switch (action) {
+        case 'up':
+            if (parseInt(top) - 30 <= 0) { validate = false; }
+            break;
+
+        case 'down':
+            if (parseInt(top) + 30 >= 600) { validate = false; }
+            break;
+
+        case 'left':
+            if (parseInt(left) - 30 <= 0) { validate = false; }
+            break;
+
+        case 'right':
+            if (parseInt(left) + 30 >= 600) { validate = false; }
+            break;
+
+    }
+
+    return validate;
 }
